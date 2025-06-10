839af2137680370f6742ff011a186ec583f46ab8
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-06-10 18:17:44 +0600

    add event handler

--- js/Home.js
+++ js/Home.js
@@ -1,1 +1,10 @@
-console.log('hello')
+//step 1 :add an event handler to the button
+document.getElementById('btn-add-money')
+.addEventListener('click', function(e) {
+    e.preventDefault() // prevent default form submission behavior
+   // step 2 : get the amount from the input field
+   const addMoneyInput = document.getElementById('input-add-money').value;
+   // step 3: get the pin number provided
+   const pinNumber = document.getElementById('input-pin-number').value;
+
+})
